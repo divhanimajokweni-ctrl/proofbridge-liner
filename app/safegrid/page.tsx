1a8f0b6e744460c48d11f3544e66833d5ec5aa38
divhanimajokweni-ctrl <divhanimajokweni-ctrl@users.noreply.github.com>
2026-07-08 18:44:50 +0000

    RC1 purge (final): remove remaining non-core files from working tree
    This commit completes the RC1 purity freeze by removing all non-core
    product files that were only git-rm'd from index in the previous commit.
    Now both index and working tree are clean.
    
    Total removed: 427 files, 76,733 lines.
    Moved to: github.com/divhanimajokweni-ctrl/vvu-ecosystem

--- app/safegrid/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import EntityLanding from "../components/EntityLanding";
-import { ENTITIES } from "../lib/entities";
-
-export default function SafeGridPage() {
-  const config = ENTITIES.find(e => e.id === "safegrid")!;
-  return <EntityLanding config={config} />;
-}
